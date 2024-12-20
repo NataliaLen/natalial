dce47a98608be5c8a8d8c9cc8e37d21225114ec6
NataliaLen <n.lendzion.577@studms.ug.edu.pl>
2024-12-20 19:49:46 +0100

    Update and rename zad 3.07c. to zad 3.07.c

--- zad 3.07.c
+++ zad 3.07.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-void prinNumbers(int n) {
+void printNumbers(int n) {
     if (n == 0) return;
-    printNumbers( n - 1);
-    printf("%d ", n;)
+    printNumbers(n - 1);
+    printf("%d ", n);
 }
+
 int main() {
     int n;
     scanf("%d", &n);
-    printNumbers(n)
+    printNumbers(n);
     return 0;
-    }
+}
